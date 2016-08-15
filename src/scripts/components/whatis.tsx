a14815621f2b54c628534d7c9e387034f79c9cfc
jeffhollan <jeffhollan@outlook.com>
2016-08-14 21:07:14 -0700

    dynamic homepage partially working

---
+++ src/scripts/components/whatis.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export class WhatIsBumpMail extends React.Component<any, {}> {
+    render() {
+        return <h1>Whatever you want it to be</h1>;
+    }
+}
--- src/scripts/main.tsx
+++ src/scripts/main.tsx
@@ -1,3 +1,16 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import startup from "./startup"
 
+import { WhatIsBumpMail } from "./components/whatis"
+
+startup();
+
+let whatis_masthead = document.getElementById('whatis_masthead');
+whatis_masthead.addEventListener('click', function() {
+    console.log('clicked');
+    ReactDOM.render(
+        <WhatIsBumpMail />,
+        document.getElementById('main')
+    );
+})
