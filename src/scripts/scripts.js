18f939b0f4b4dc6d7b4c8641fea6b3986423e303
Jeff Hollan <jeffhollan@outlook.com>
2016-08-28 20:51:52 -0700

    working on auth0 profile

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -24,6 +24,22 @@ $(document)
       lock.show();
     });
 
+    document.getElementById('test').addEventListener('click', function () {
+      var getFoos = fetch('https://jehollan.azure-api.net/bump/echo', {
+        headers: {
+          'x-auth-token': localStorage.getItem('id_token')
+        },
+        method: 'GET',
+        cache: false
+      });
+
+      getFoos.then(function (response) {
+        response.json().then(function (res) {
+          console.log('response:', res);
+        });
+      });
+    });
+
 
     //Initialize the homepage
     var lock = new Auth0Lock('pCXBrgxHBq7l99ROrRKKBslbdjUYAktO', 'hollan.auth0.com', {
@@ -32,6 +48,19 @@ $(document)
       }
     }
     );
+
+    lock.on("authenticated", function (authResult) {
+      lock.getProfile(authResult.idToken, function (error, profile) {
+        if (error) {
+          // Handle error
+          return;
+        }
+
+        localStorage.setItem('id_token', authResult.idToken);
+        localStorage.setItem('profile', JSON.stringify(profile));
+        console.log(JSON.stringify(profile));
+      });
+    });
 
   })
   ;
